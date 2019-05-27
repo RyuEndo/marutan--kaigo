ada6d922dbb743f35c27ea486c7cf018bd3e443b
itokun12 <nao7120ya@gmail.com>
2019-05-27 22:04:12 +0900

    controller: use copied code

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,12 +1,24 @@
 class LineBotController < ApplicationController
-  # disable csrf auth of callback action
+  require 'line/bot'
+
   protect_from_forgery :except => [:callback]
-  before_action :validate_signature, only: %i(callback)
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
 
   def callback
     body = request.body.read
 
-    events = line_client.parse_events_from(body)
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      head :bad_request
+    end
+
+    events = client.parse_events_from(body)
 
     events.each { |event|
       case event
@@ -52,15 +64,5 @@ class LineBotController < ApplicationController
     }
 
     head :ok
-  end
-
-  private
-
-  def validate_signature
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless line_client.validate_signature(body, signature)
-      head :bad_request
-    end
   end
 end
