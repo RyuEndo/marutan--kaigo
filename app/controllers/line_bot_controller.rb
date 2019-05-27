0c5913b243e11b4d1dfe4d445cc1e574e79057db
itokun12 <nao7120ya@gmail.com>
2019-05-27 21:58:27 +0900

    routes, controller: add callback api with sample code

---
+++ app/controllers/line_bot_controller.rb
@@ -0,0 +1,66 @@
+class LineBotController < ApplicationController
+  # disable csrf auth of callback action
+  protect_from_forgery except: %i(:callback)
+  before_action :validate_signature, only: %i(callback)
+
+  def callback
+    body = request.body.read
+
+    events = line_client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: "text",
+            text: "Select your favorite food category or send me your location!",
+            quickReply: {
+              items: [
+                {
+                  type: "action",
+                  imageUrl: "https://example.com/sushi.png",
+                  action: {
+                    type: "message",
+                    label: "Sushi",
+                    text: "Sushi"
+                  }
+                },
+                {
+                  type: "action",
+                  imageUrl: "https://example.com/tempura.png",
+                  action: {
+                    type: "message",
+                    label: "Tempura",
+                    text: "Tempura"
+                  }
+                },
+                {
+                  type: "action",
+                  action: {
+                    type: "location",
+                    label: "Send location"
+                  }
+                }
+              ]
+            }
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    }
+
+    head :ok
+  end
+
+  private
+
+  def validate_signature
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless line_client.validate_signature(body, signature)
+      head :bad_request
+    end
+  end
+end
