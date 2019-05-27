3499b96c2af0a45bfc37804ba13148fec397b6b8
itokun12 <nao7120ya@gmail.com>
2019-05-27 21:58:12 +0900

    controller: add line_client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  require 'line/bot'
+
+  private
+
+  def line_client
+    @line_client ||= Line::Bot::Client.new { |config|
+      config.channel_token  = ENV['LINE_CHANNEL_TOKEN']
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+    }
+  end
 end
