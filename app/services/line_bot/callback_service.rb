728d34fe731bf93f8fa8aa0dc12bbf0230b5573c
(ryu endo) <(3pieceryu@gmail.com)>
2019-05-31 21:01:46 +0900

    介護用に変更

--- app/services/line_bot/callback_service.rb
+++ app/services/line_bot/callback_service.rb
@@ -26,21 +26,30 @@ module LineBot
       events.each { |event|
         case event
         when Line::Bot::Event::Follow
-          client.reply_message(event['replyToken'], [appreciate_following, ask_firstquestion])
+          client.reply_message(event['replyToken'], [appreciate_following, ask_area])
         when Line::Bot::Event::Message
           case event.type
           when Line::Bot::Event::MessageType::Text
             case event.message['text']
-            when 'い', 'う', 'え', 'お'
-              client.reply_message(event['replyToken'], [appreciate, ask_secoundquestion])
-            when 'き', 'く', 'け', 'こ'
-              client.reply_message(event['replyToken'], [appreciate, ask_thirdquestion])
-            when 'し', 'す', 'せ', 'そ'
-              client.reply_message(event['replyToken'], [appreciate, ask_firstquestion])
-			when 'あ','か','さ'
-              client.reply_message(event['replyToken'], [appreciate, carousel])
+				
+		　　when '地域の選択に戻る','他の施設を探す'
+			  client.reply_message(event['replyToken'], [appreciate, ask_area])
+            when '北海道', '東北', '関東', '中部', '近畿', '中国', '近畿', '九州','都道府県の選択に戻る'
+              client.reply_message(event['replyToken'], [appreciate, ask_second_area])
+            when '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県','費用の選択に戻る'
+              client.reply_message(event['replyToken'], [appreciate, ask_cost])
+            when '一時金なし', '50万円以下', '100万円以下', '200万円以下', '300万円以下','入居希望時期の選択に戻る'
+              client.reply_message(event['replyToken'], [appreciate, ask_period])
+			when '1週間以内','1か月以内','3か月以内','6か月以内','1年以内','それ以降','未定'
+              client.reply_message(event['replyToken'], [appreciate, ask_condition])
+		    when '自立','要支援','要介護','認知症相談可','不明'
+              client.reply_message(event['replyToken'], [appreciate, ask_favorite])
+			when 'ある','ない'
+              client.reply_message(event['replyToken'], [appreciate, propose_home])
+			when 'する','しない'
+              client.reply_message(event['replyToken'], [appreciate, ask_after])
             else
-              client.reply_message(event['replyToken'], [not_implemented,ask_firstquestion])
+              client.reply_message(event['replyToken'], [not_implemented,ask_after])
             end
           end
         end
--- app/services/line_bot/message.rb
+++ app/services/line_bot/message.rb
@@ -14,50 +14,149 @@ module LineBot
       }
     end
 
-    def ask_firstquestion
+    def ask_area
       {
         type: 'text',
-        text: '一つ目の質問です.',
+        text: 'まずは介護施設を探す地域を教えてください！',
         quickReply: {
           items: [
             {
               type: 'action',
               action: {
                 type: 'message',
-                label: 'あ',
-                text: 'あ'
+                label: '北海道',
+                text: '北海道'
               }
             },
             {
               type: 'action',
               action: {
                 type: 'message',
-                label: 'い',
-                text: 'い'
+                label: '東北',
+                text: '東北'
               }
             },
 			{
               type: 'action',
               action: {
                 type: 'message',
-                label: 'う',
-                text: 'う'
+                label: '関東',
+                text: '関東'
               }
             },
 			{
               type: 'action',
               action: {
                 type: 'message',
-                label: 'え',
-                text: 'え'
+                label: '中部',
+                text: '中部'
               }
             },
 			{
               type: 'action',
               action: {
                 type: 'message',
-                label: 'お',
-                text: 'お'
+                label: '近畿',
+                text: '近畿'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '中国',
+                text: '中国'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '四国',
+                text: '四国'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '九州',
+                text: '九州'
+              }
+            }
+          ]
+        }
+      }
+    end
+	  
+	def ask_second_area
+      {
+        type: 'text',
+        text: '次に都道府県を教えてください！',
+        quickReply: {
+          items: [
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '茨城県',
+                text: '茨城県'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '栃木県',
+                text: '栃木県'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '群馬県',
+                text: '群馬県'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '埼玉県',
+                text: '埼玉県'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '千葉県',
+                text: '千葉県'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '東京都',
+                text: '東京都'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '神奈川県',
+                text: '神奈川県'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '地域の選択に戻る',
+                text: '地域の選択に戻る'
               }
             }
           ]
@@ -65,50 +164,58 @@ module LineBot
       }
     end
 
-    def ask_secoundquestion
+    def ask_cost
       {
         type: 'text',
-        text: '二つ目の質問です',
+        text: '想定している費用を教えてください。まずは入居一時金についてです。',
         quickReply: {
           items: [
             {
               type: 'action',
               action: {
                 type: 'message',
-                label: 'か',
-                text: 'か'
+                label: '一時金なし',
+                text: '一時金なし'
               }
             },
             {
               type: 'action',
               action: {
                 type: 'message',
-                label: 'き',
-                text: 'き'
+                label: '50万円以下',
+                text: '50万円以下'
               }
             },
 			{
               type: 'action',
               action: {
                 type: 'message',
-                label: 'く',
-                text: 'く'
+                label: '100万円以下',
+                text: '100万円以下'
               }
             },
 			{
               type: 'action',
               action: {
                 type: 'message',
-                label: 'け',
-                text: 'け'
+                label: '200万円以下',
+                text: '200万円以下'
               }
             },
 			{
               type: 'action',
               action: {
                 type: 'message',
-                label: 'こ',
-                text: 'こ'
+                label: '300万円以下',
+                text: '300万円以下'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '都道府県の選択に戻る',
+                text: '都道府県の選択に戻る'
               }
             }
           ]
@@ -116,56 +223,230 @@ module LineBot
       }
     end
 
-    def ask_thirdquestion
+    def ask_period
       {
         type: 'text',
-        text: '三つ目の質問です',
+        text: '入居希望時期は決まっていますか？',
         quickReply: {
           items: [
             {
               type: 'action',
               action: {
                 type: 'message',
-                label: 'さ',
-                text: 'さ'
+                label: '1週間以内',
+                text: '1週間以内'
               }
             },
             {
               type: 'action',
               action: {
                 type: 'message',
-                label: 'し',
-                text: 'し'
+                label: '1か月以内',
+                text: '1か月以内'
               }
             },
 			{
               type: 'action',
               action: {
                 type: 'message',
-                label: 'す',
-                text: 'す'
+                label: '3か月以内',
+                text: '3か月以内'
               }
             },
 			{
               type: 'action',
               action: {
                 type: 'message',
-                label: 'せ',
-                text: 'せ'
+                label: '6カ月以内',
+                text: '6カ月以内'
               }
             },
 			{
               type: 'action',
               action: {
                 type: 'message',
-                label: 'そ',
-                text: 'そ'
+                label: '1年以内',
+                text: '1年以内'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: 'それ以降',
+                text: 'それ以降'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '未定',
+                text: '未定'
+              },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '費用の選択に戻る',
+                text: '費用の選択に戻る'
+              }
+            }
+            }
+          ]
+        }
+      }
+    end
+	  
+	def ask_condition
+      {
+        type: 'text',
+        text: 'お探しの施設の入居希望条件を教えてください！',
+        quickReply: {
+          items: [
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '自立',
+                text: '自立'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '要支援',
+                text: '要支援'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '要介護',
+                text: '要介護'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '認知症相談可',
+                text: '認知症相談可'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '不明',
+                text: '不明'
+              }
+            },
+			{
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '入居希望時期の選択に戻る',
+                text: '入居希望時期の選択に戻る'
               }
             }
           ]
         }
       }
     end
+	  
+	def ask_condition
+      {
+        type: 'text',
+        text: '医療・感染症などの特別な条件はありますか？',
+        quickReply: {
+          items: [
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: 'ある',
+                text: 'ある'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: 'ない',
+                text: 'ない'
+              }
+            }
+          ]
+        }
+      }
+    end
+	  
+	def propose_home
+      {
+        type: 'text',
+        text: 'あなたのお探しの条件にぴったりの介護施設を探してきました！一括で資料請求しますか？(もう少し詰める必要がある)',
+        quickReply: {
+          items: [
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: 'する',
+                text: 'する'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: 'しない',
+                text: 'しない'
+              }
+            }
+          ]
+        }
+      }
+    end
+	  
+    def ask_after
+      {
+        type: 'text',
+        text: '他に何かできることはありますか？',
+        quickReply: {
+          items: [
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '他の施設を探す',
+                text: '他の施設を探す'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '見学の仕方について相談する',
+                text: '見学の仕方について相談する'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '見学の仕方について相談する',
+                text: '見学の仕方について相談する'
+              }
+            }
+          ]
+        }
+      }
+    end
+	  
+	  
 
     def not_implemented
       {
@@ -173,6 +454,7 @@ module LineBot
         text: 'まだ対応できません'
       }
     end
+	  
     def carousel
     {
     "type": "template",
